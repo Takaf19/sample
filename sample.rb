c11d0639087aa96cbbfc9b62c793e55227ac8d0b
Takaf19 <tk.f19ms@gmail.com>
2020-02-24 17:50:47 +0900

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -1,5 +1,21 @@
-def hello
-  p "hello, GitHub Desktop."
+def fizzbuzz(stop)
+  i = 1
+  while i <= stop
+    case True
+      when i % 3 == 0 then
+        puts "Fizz"
+      when i % 5 == 0  then
+        puts "Buzz"
+      when i % 15 == 0 then
+        puts "FizzBuzz"
+      else
+        puts i
+    end
+    i += 1
+  end
 end
 
-hello
+ puts "カウント数を入力"
+ count = gets.to_i
+
+ fizzbuzz(count)
